6a54ef7cad804edabf29cfefbfcfa120b006ef60
katoyuji <kato.yuji@gmail.com>
2013-12-03 16:31:09 +0100

    test de commit

---
+++ test.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Yuji Kato
+#
+# Created:     17/05/2013
+# Copyright:   (c) Yuji Kato 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import arcpy
+
+fc = "C:/damien/TEST_IST.shp"
+
+rows = arcpy.UpdateCursor(fc)
+for row in rows:
+    activites = row.ALL_ACT_ID.split(":")
+    i = 0
+    conf = 0
+    while i<(len(activites)-1):
+        j = i+1
+        a = activites[i]
+        while j<len(activites):
+            b = activites[j]
+            if (a=="E113" and b=="E114") or (b=="E113" and a=="E114"):
+                conf += 1
+            j += 1
+        i += 1
+    row.conflit = conf
+    rows.updateRow(row)
